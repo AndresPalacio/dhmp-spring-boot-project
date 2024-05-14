43b04dcd0ed4198e15d311a1dc9364df2a98abbb
王俊 <1595890874@qq.com>
2024-05-14 11:34:59 +0800

    [refactor] 数据导出组件重构优化

--- dhmp-spring-boot-project/dhmp-spring-boot-autoconfigure/src/main/java/com/zznode/dhmp/boot/autoconfigure/export/DhmpExportAutoConfiguration.java
+++ dhmp-spring-boot-project/dhmp-spring-boot-autoconfigure/src/main/java/com/zznode/dhmp/boot/autoconfigure/export/DhmpExportAutoConfiguration.java
@@ -1,11 +1,14 @@
 package com.zznode.dhmp.boot.autoconfigure.export;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zznode.dhmp.boot.autoconfigure.lov.LovAutoConfiguration;
-import com.zznode.dhmp.export.ExportAspect;
-import com.zznode.dhmp.export.ExportConfig;
+import com.zznode.dhmp.export.ExportContext;
+import com.zznode.dhmp.export.config.ExportConfigProperties;
+import com.zznode.dhmp.export.config.ExportConfiguration;
+import com.zznode.dhmp.export.config.ExportConfigurer;
 import com.zznode.dhmp.export.converter.LovConverter;
-import com.zznode.dhmp.export.exporter.DefaultExporterFactory;
-import com.zznode.dhmp.export.exporter.ExporterFactory;
+import com.zznode.dhmp.export.support.exporter.DefaultExporterFactory;
+import com.zznode.dhmp.export.support.exporter.ExporterFactory;
 import com.zznode.dhmp.export.utils.ExportHelper;
 import com.zznode.dhmp.lov.client.LovClient;
 import org.springframework.beans.factory.ObjectProvider;
@@ -13,6 +16,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,54 +28,58 @@ import org.springframework.context.annotation.Import;
  * @author 王俊
  * @date create in 2023/8/30
  */
-@AutoConfiguration(after = LovAutoConfiguration.class)
-@ConditionalOnClass({ExportAspect.class})
-@Import({DhmpExportWebConfiguration.class})
-public class DhmpExportAutoConfiguration  {
+@AutoConfiguration(after = {LovAutoConfiguration.class, JacksonAutoConfiguration.class})
+@ConditionalOnClass({ExportContext.class})
+@Import({ExportConfiguration.class})
+public class DhmpExportAutoConfiguration {
 
-//    @Bean
-//    ExportInitializingBean exportInitializingBean() {
-//        return new ExportInitializingBean();
-//    }
 
-    @ConfigurationProperties("dhmp.export")
     @Bean
-    @ConditionalOnMissingBean
-    public ExportConfig exportConfig() {
-        return new ExportConfig();
+    @ConfigurationProperties("dhmp.export")
+    public ExportConfigProperties exportConfigProperties() {
+        return new ExportConfigProperties();
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public ExportHelper exportHelper(ExportConfig exportConfig,
-                                     ObjectProvider<LovConverter> lovConverter) {
+    public ExportHelper exportHelper(ObjectProvider<LovConverter> lovConverters, ObjectProvider<ObjectMapper> objectMappers) {
         ExportHelper exportHelper = new ExportHelper();
-        exportHelper.setExportConfig(exportConfig);
-        exportHelper.setLovConverter(lovConverter.getIfAvailable());
+        lovConverters.ifAvailable(exportHelper::setLovConverter);
+        objectMappers.ifAvailable(exportHelper::setObjectMapper);
         return exportHelper;
     }
 
-    @Bean
-    @ConditionalOnMissingBean
-    public ExportAspect exportAspect(ExportHelper exportHelper,
-                                     ExporterFactory exporterFactory) {
-        ExportAspect exportAspect = new ExportAspect();
-        exportAspect.setExportFactory(exporterFactory);
-        exportAspect.setExportHelper(exportHelper);
-        return exportAspect;
-    }
 
     @Bean
     @ConditionalOnMissingBean
-    public ExporterFactory exporterFactory(ExportConfig exportConfig) {
+    public ExporterFactory exporterFactory(ExportConfigProperties exportConfig, ExportHelper exportHelper) {
         DefaultExporterFactory exporterFactory = new DefaultExporterFactory();
         exporterFactory.setExportConfig(exportConfig);
+        exporterFactory.setExportHelper(exportHelper);
         return exporterFactory;
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnMissingBean(ExportConfigurer.class)
+    static class ExporterFactoryConfiguration implements ExportConfigurer {
+
+        private final ObjectProvider<ExporterFactory> exporterFactory;
+
+        ExporterFactoryConfiguration(ObjectProvider<ExporterFactory> exporterFactory) {
+            this.exporterFactory = exporterFactory;
+        }
+
+        @Override
+        public ExporterFactory getExporterFactory() {
+            return exporterFactory.getIfAvailable();
+        }
+
     }
 
 
     @ConditionalOnClass(LovClient.class)
     @ConditionalOnBean(LovClient.class)
+    @ConditionalOnMissingBean(LovConverter.class)
     @Configuration
     static class LovConverterRegisterConfiguration {
 
